c6146dd5e6dd7d1c2762b505b490d955f349672c
Gabriel Suldine <82013387+Gabriel-Suldine@users.noreply.github.com>
2022-07-27 20:59:53 -0400

    Exercício em aula

---
+++ Ex01.java
@@ -0,0 +1,24 @@
+package exerciciosEmAula;
+
+/**
+ *
+ * @author SULDINE
+ */
+public class Ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i % 2 == 0) {
+                System.out.println("Número "+i);        
+            }
+
+        }
+    }
+
+}
+
---
+++ Ex02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerciciosEmAula;
+
+/**
+ *
+ * @author 03569198200
+ */
+public class Ex02 {
+
+    public static void main(String[] args) {
+        int[] numeros = {14, 9, 7};
+        int maior = 0;
+        for (int valor : numeros) {
+            if (valor > maior) {
+                maior = valor;
+            }
+
+        }
+         System.out.println("Números maior é "+maior);
+    }
+}
